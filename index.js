3c7b98ff532e340ad6b9929f18e895b74454cf6e
ashok kumar <ash1982ok@gmail.com>
2021-03-09 03:19:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * This function adds two variable and returns its summation
+ *
+ * @param {number} num1 first number for summation
+ * @param {number} num2 second number for summation
+ * @returns {number} summation of the two number provided
+ * 
+ * 
+ * @example 
+ * 
+ * addition (5,10)
+ * 
+ */
+
+function addition (num1, num2) {
+    return num1 + num2;
+}
+
+function multiplication (num1, num2) {
+    return num1 * num2;
+}
